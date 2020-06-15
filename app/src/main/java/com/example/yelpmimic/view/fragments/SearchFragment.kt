c1e5ebde0e04bf49212233afa2917d6186fe4214
cthoughtz <cthoughtz@gmail.com>
2020-06-15 13:32:45 -0500

    Implement Random header image and status bar color when app is launched

--- app/src/main/java/com/example/yelpmimic/view/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/yelpmimic/view/fragments/SearchFragment.kt
@@ -1,13 +1,18 @@
 package com.example.yelpmimic.view.fragments
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.yelpmimic.R
+import com.example.yelpmimic.view.activities.MainActivity
 import com.google.android.material.appbar.AppBarLayout
 import kotlinx.android.synthetic.main.fragment_search.*
+import java.util.*
 
 
 /**
@@ -27,10 +32,144 @@ class SearchFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        randomHeader()
        setSearchBar()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun randomHeader() {
+
+        val randomNumber = Random()
+        val statusBarColor = activity!!.window
+        val number = randomNumber.nextInt(30)
+
+        when (number) {
+            0 ->  {
+                mainHeaderImage.setImageResource(R.drawable.art)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.art_header)
+            }
+            1 -> {
+                mainHeaderImage.setImageResource(R.drawable.auto)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.auto_header)
+                }
+            2 ->  {
+                mainHeaderImage.setImageResource(R.drawable.bars)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.bar_header)
+            }
+            3 ->  {
+                mainHeaderImage.setImageResource(R.drawable.beauty)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.beauty_header)
+            }
+            4 ->  {
+                mainHeaderImage.setImageResource(R.drawable.education)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.education_header)
+            }
+            5 ->  {
+                mainHeaderImage.setImageResource(R.drawable.educationtwo)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.education_two_header)
+            }
+            6 ->  {
+                mainHeaderImage.setImageResource(R.drawable.farmone)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.farm_one)
+
+            }
+            7 ->  {
+                mainHeaderImage.setImageResource(R.drawable.farmtwo)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.farm_two)
+            }
+            8 ->  {
+                mainHeaderImage.setImageResource(R.drawable.fashion)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.fashion)
+            }
+            9 ->  {
+                mainHeaderImage.setImageResource(R.drawable.fitness)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.fitness_header)
+
+            }
+            11 ->  {
+                mainHeaderImage.setImageResource(R.drawable.fitnesstwo)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.fitness_header_two)
+            }
+            12 ->  {
+                mainHeaderImage.setImageResource(R.drawable.food)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.food_header)
+            }
+            13 ->  {
+                mainHeaderImage.setImageResource(R.drawable.foodtwo)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.food_header_two)
+            }
+            14 ->  {
+                mainHeaderImage.setImageResource(R.drawable.health)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.health_header)
+            }
+            15 -> {
+                mainHeaderImage.setImageResource(R.drawable.hotels)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.hotel_header)
+            }
+            16 -> {
+                mainHeaderImage.setImageResource(R.drawable.italian)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.italian_header)
+            }
+            17 -> {
+                mainHeaderImage.setImageResource(R.drawable.localservices)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.local_service_header)
+
+            }
+            18 -> {
+                mainHeaderImage.setImageResource(R.drawable.medicalspa)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.medical_spa)
+            }
+            19 -> {
+                mainHeaderImage.setImageResource(R.drawable.medicalspatwo)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.medical_spa_two)
+            }
+            20 -> {
+                mainHeaderImage.setImageResource(R.drawable.medicalspathree)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.medical_spa_three)
+            }
+            21 -> {
+                mainHeaderImage.setImageResource(R.drawable.museum)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.museum_header)
+            }
+            22 -> {
+                mainHeaderImage.setImageResource(R.drawable.nightlife)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.night_life_header)
+            }
+            23 -> {
+                mainHeaderImage.setImageResource(R.drawable.pets)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.pet_header)
+            }
+            24 -> {
+                mainHeaderImage.setImageResource(R.drawable.physicians)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.physicians_header)
+            }
+            25 -> {
+                mainHeaderImage.setImageResource(R.drawable.professional)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.professional_header)
+            }
+            26 -> {
+                mainHeaderImage.setImageResource(R.drawable.realestate)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.real_estate_header)
+            }
+            27 -> {
+                mainHeaderImage.setImageResource(R.drawable.resaurant)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.resaurant)
+            }
+            28 -> {
+                mainHeaderImage.setImageResource(R.drawable.restauranttwo)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.restaurant_two)
+            }
+            29 -> {
+                mainHeaderImage.setImageResource(R.drawable.transport)
+                statusBarColor.statusBarColor = ContextCompat.getColor(requireContext(),R.color.airplain_header)
+            }
+
+        }
+
     }
 
     private fun setSearchBar() {
@@ -46,13 +185,12 @@ class SearchFragment : Fragment() {
                 }
                 if (scrollRange + verticalOffset == 0) {
 
-                   // toolbar.setTitle("This is a test")
+
                     searchView.visibility = View.INVISIBLE
                     searchViewToolbar.visibility = View.VISIBLE
                     isShow = true
                 } else if (isShow) {
 
-                 //   toolbar.setTitle(" ")
                     searchView.visibility = View.VISIBLE
                     searchViewToolbar.visibility = View.INVISIBLE
                     isShow = false
